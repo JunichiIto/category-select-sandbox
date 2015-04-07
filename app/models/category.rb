282e7bc2d49fc8518703821c0b7f0097005d32e8
Junichi Ito <jit@sonicgarden.jp>
2015-04-08 06:59:25 +0900

    Remove unnecessary relationships.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,3 @@
 class Category < ActiveRecord::Base
-  has_many :items
   has_many :sub_categories, ->{ order(:code) }, class_name: 'Category', foreign_key: 'parent_id'
-  belongs_to :categories, class_name: 'Category', foreign_key: 'parent_id'
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   belongs_to :sub_category, class_name: 'Category', foreign_key: :sub_category_id
-  has_many :sub_categories, class_name: 'Category', foreign_key: :sub_category_id
 
   validates :title, presence: true
 end
