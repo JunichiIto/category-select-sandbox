5faf8b6f337eb6b4bac5e16325b6705d4a26b1f1
Junichi Ito <jit@sonicgarden.jp>
2015-04-08 05:08:14 +0900

    Add expectations.

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -5,10 +5,24 @@ feature 'Items', type: :feature do
     visit root_path
     click_on 'New Item'
 
-    fill_in 'Title', with: 'バット'
     select 'スポーツ', from: 'Category'
     select 'スポーツ用品', from: 'Sub category'
     click_on 'Save'
+
+    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_select('Category', selected: 'スポーツ')
+    expect(page).to have_select('Sub category', selected: 'スポーツ用品')
+
+    fill_in 'Title', with: 'バット'
+    click_on 'Save'
+
     expect(page).to have_content 'Item was successfully created.'
+    expect(page).to have_content 'バット'
+    expect(page).to have_content 'スポーツ'
+    expect(page).to have_content 'スポーツ用品'
+
+    click_on 'Edit'
+    expect(page).to have_select('Category', selected: 'スポーツ')
+    expect(page).to have_select('Sub category', selected: 'スポーツ用品')
   end
 end
