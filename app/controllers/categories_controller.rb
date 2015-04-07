66d20ba7f3d013f9ab305665e30c9647ebd076c2
Junichi Ito <jit@sonicgarden.jp>
2015-04-08 05:15:40 +0900

    Specify default order.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def sub_categories
     category = Category.find(params[:category_id])
-    render json: category.sub_categories.order(:code)
+    render json: category.sub_categories
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :items
-  has_many   :sub_categories, class_name: 'Category', foreign_key: 'parent_id'
+  has_many :sub_categories, ->{ order(:code) }, class_name: 'Category', foreign_key: 'parent_id'
   belongs_to :categories, class_name: 'Category', foreign_key: 'parent_id'
 end
